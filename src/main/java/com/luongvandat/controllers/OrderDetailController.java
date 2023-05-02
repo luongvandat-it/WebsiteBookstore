d836b3687ab1038cccfa4eeee8f2184fbbdd2386
luongvandat <luongvandat.it@gmail.com>
2023-05-02 14:23:50 +0700

    Add order services

--- src/main/java/com/luongvandat/controllers/OrderDetailController.java
+++ src/main/java/com/luongvandat/controllers/OrderDetailController.java
@@ -1,11 +1,23 @@
 package com.luongvandat.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.luongvandat.entities.OrderDetail;
+import com.luongvandat.services.OrderDetailServices;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/orderDetails")
 public class OrderDetailController {
+    private final OrderDetailServices orderDetailServices;
+
+    public OrderDetailController(OrderDetailServices orderDetailServices) {
+        this.orderDetailServices = orderDetailServices;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addOrderDetail(@RequestBody OrderDetail orderDetail) {
+        orderDetailServices.add(orderDetail);
+        return ResponseEntity.ok("SUCCESS: Order added successfully !");
+    }
 }
